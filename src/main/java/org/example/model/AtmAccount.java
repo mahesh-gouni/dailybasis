3c8bc46428953f530240bb11b88c4a8e0dc6d169
gouni <gounimahesh9000@gmail.com>
2024-12-07 12:41:20 +0530

    victus

--- src/main/java/org/example/model/AtmAccount.java
+++ src/main/java/org/example/model/AtmAccount.java
@@ -1,4 +1,30 @@
 package org.example.model;
 
 public class AtmAccount {
+    private String name;
+    private int age;
+    private double amount;
+
+
+
+    public void setName(String name){
+        this.name   =name;
+    }
+    public String getName(){
+        return name;
+    }
+
+     public void setAge(int age){
+        this.age=age;
+     }
+     public int getAge(){
+        return age;
+     }
+
+     public void setAmount(double amount){
+        this.amount =amount;
+     }
+     public double getAmount(){
+        return amount;
+     }
 }
